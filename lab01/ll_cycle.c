636b47d9950da18e9ea83dc111747ccb336fb181
chen quan <chenquan5616@gmail.com>
2025-01-29 23:59:30 +0800

    finish lab01, some problems remain

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,14 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise=head;
+    node *hare=head;
+    while(hare&&hare->next){
+        tortoise=tortoise->next;
+        hare=hare->next;
+        hare=hare->next;
+        if(tortoise==hare)
+        return 1;
+    }
     return 0;
 }
